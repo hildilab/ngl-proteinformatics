82bb9ba6be7353e77eea5f6a0f9747718b37e5de
jochen <jochen@lesum>
2015-12-21 17:52:59 +0100

    website changes II

--- www/manual.php
+++ www/manual.php
@@ -19,40 +19,30 @@
            
     <table><tbody>
     <tr>
-	<td ><img src="pictures/website_overview_inlay.png"  alt="logo"></td>
+	<td ><img src="pictures/website_overview.png"  width="80%" height="80%" alt="logo"></td>
         <td > <h3>Start</h3><br><a>
-		The SuperLooper2 start display has two different panels. In the upper left corner are the general
-		controls for the NGL viewer located in dropdown menus. The right panel contains the SuperLooper2
-		controls, including the opportunity to start new jobs, to display an example or to visualize
-		previously calculated and downloaded results. <br>  </a></td>
+		The SL2 start display has two different panels. In the upper left
+		corner you find the general controls for the NGL viewer, located
+		in dropdown menus. The right panel contains the SL2 controls: a
+		button to start new jobsor to load an example. <br>  </a></td>
         
     </tr>   
     <tr>    
-        <td><img src="pictures/new_job.png"  alt="logo"></td>
+        <td><img src="pictures/new_jobh.png" alt="logo"></td>
        <td><h3>New job</h3> <a>
-	   Input requirements for a SuperLooper2 job are a protein structure with missing segments,
-	   two stem residues defining the position of the loop and the amino acids sequence of the missing
-	   segment. A fragment search has a runtime of up to 30 seconds and displays afterwards 
-	   the results automatically.</a></td>
+	   To start a new SL2 job you have to provide  a coordinate file of a protein structure
+	   in PDB format. The two stem residues flanking a missing segment or a segment that
+	   should be remodelled have to be typed in a formular or selected from the NGL
+	   visualization by mouse selection. Finally, the amino acids sequence of the missing
+	   segment has to be provided. Depening on fragment length, a fragment search may take
+	   up to 30 seconds. When the job is finished, the results are displayed automatically.</a></td>
     </tr>
     <tr>
-        <td><img src="pictures/result_table.png" alt="logo"></td>
+        <td><img src="pictures/result_table_h.png" alt="logo"></td>
         <td><h3>Results</h3> <a>
-		Subsequent to finishing the search, the results are displayed in the NGL viewer and the loops with 
-		additional informations are provided in a table at the right panel.<br>
-        For each loop its ID, score, clash score and further informations about the template like its PDB ID,
-        sequence, the sequence identity of the template compaired to the user-defined input sequence and
-		the position of the loop in the template structure are listed. 
-		Each structure including the resulting loop
-		can be downloaded individually by clicking the download symbol at each result.
-		In addition, all found loops are downloadable for a further external analysis.<br>
-		By selecting a result, the loop inserted into the protein is displayed. 
-		The found loops can all be displayed at the same time to analyze the conformational space
-		that is covered by all found loops.<br>
-        The loop can be colored according to its score, sequence identity or clash
-		score by selecting the corresponding color scheme
-        from the dropdown menu. Additionally, the representation of the structure and
-		the loop (collapsed underneath the result table) can be changed as explained in the documentation of the NGL viewer.<br>        
+		The top candidate loop is automatically displayed in the NGL viewer together with the uploaded protein (model). Other loops can be selected from the list of 50 candidates by visualization or by using the information provided in the table at the right panel for guidance. This table includes the following information for each candidate loop: an ID (?), a score (?), a clash score, the PDB ID and the position of the fragment in the template protein, and a comparison of the user-defined input sequence with the template sequence (sequence similarity). 
+By selecting a result, the loop is displayed together with the uploaded protein. The complete list of loop candidates can be displayed simultaneously to visualize the conformational space of the loop. The loop can be colored according to its score, sequence identity or clash score by selecting the corresponding color scheme from the dropdown menu. Additionally, the representation of the structure and the loop (collapsed underneath the result table) can be changed as explained in the separate documentation of the NGL viewer. 
+The completed structure (initial model plus selected loop) can be downloaded by clicking the download symbol. Alternatively, the complete list of loops can be downloaded for a further analysis.       
         </a></td>
      </tr>
     
--- www/methods.php
+++ www/methods.php
@@ -14,62 +14,70 @@
     <div id="content" class="content">
         <h1>Methods</h1>
         <h2>Database</h2>
-        <p>To construct the database all overlapping fragments with a
-        length of 3 to 35 were extracted from a collection of about
-        100.000 protein structures currently deposited in the PDB.
-        For each fragment the following informations are stored in the
-        database: Sequence, PDB identifier, location in the protein
-        and a geometrical fingerprint that characterizes each fragment.
-        The total number of fragments sum up to over 700 million.
-		The number of available fragments is decreasing with increasing
-        number of residues. This is caused by the fact that a smaller
-        number of long fragments can be extracted from a structure, i.e.
-        for the fragment length of 3 amino acids 23 million fragments
-        are available, while for a fragment length of 35 this number declines
-        to approximately 18 million.</p>
+        <p>To construct the database all overlapping fragments with a length
+	of 3 to 35 were extracted from a collection of more than 100.000 protein
+	structures currently deposited in the PDB. For each fragment the following
+	information is stored in the database: Sequence, PDB identifier, location
+	in the protein and a geometrical fingerprint (see below). The total number
+	of fragments sum up to over 700 million. The number of available fragments
+	is decreasing with increasing number of residues. This is caused by the fact
+	that a smaller number of long fragments can be extracted from a structure, e.g.
+	for the fragment length of 3 amino acids 23 million fragments are available,
+	while for a fragment length of 35 this number declines to approximately 18 million.</p>
         <div align=center>
         <img src="pictures/database_both.png" width="40%" height="40%" alt="logo">
          </div>   
         <h2>Geometric fingerprint</h2>
-        <p> The geometrical fingerprint matching is used to evaluate
-		 the sterical fit of the stem atoms of the N- and C-termini of each database fragment
-		 to the C- and N-terminal stem atoms of a gap in a protein structure. 
-		The geometrical fingerprints of both, the termini of each Superlooper2
-        fragment and the N- and C-terminal stem atoms of a requested segment,
-        is composed by the distance between their N- and C-terminal stem atoms and
-        three angles defining the relative orientation of the stem atoms (Fig. 2). This fingerprint
-        is a slight alteration of the fingerprint used in our older publication.
-        The previous combination of two distances and two angles was adjusted by the usage of one distance
-        and three angles. Analysis of the previous fingerprint revealed that the
-        score was slightly biased towards that residue where the angle was measured.
-        By using angles on both stem residues, the resulting score does not privilege
-        the fit of the candidate fragment to one stem residue over the other any more.</p>
-        <div align=center>
-        <img src="pictures/fingerprint.png" width="40%" height="40%" alt="logo">
-        </div>    
-         <p><i><b>Fig 2: Schematic representation of the geometrical fingerprint:</b>
-         The geometrical fingerprint is characterized by the distance d between
-         the N-terminal C atom and the C-terminal N atom and the following three angles:
+        <p> The geometrical fingerprint matching is used to evaluate the sterical fit
+	of the stem atoms of the N- and C-termini of each database fragment to the C-
+	and N-terminal stem atoms of a gap in a protein structure. Both geometrical
+	fingerprints are composed by the distance between the N- and C-terminal stem
+	atoms and three angles defining the relative orientation of the stem atoms.The geometrical
+	fingerprint is characterized by the distance d between the N-terminal C atom and the
+	C-terminal N atom and the following three angles:
          &alpha; defined by the line between C&alpha;<sup>(N)</sup>  and C<sup>(N)</sup>
          and d, &beta; is spanned by the line between N<sup>(C)</sup>  and C&alpha;<sup>(C)</sup>
          and d,  &gamma;: the angle between the two planes A (defined by C&alpha;<sup>(N)</sup>,
          C<sup>(N)</sup>) and N<sup>(C)</sup>) and B (C&alpha;<sup>(C)</sup>, C<sup>(N)</sup>
-         and N<sup>(C)</sup>).</i></p>   
+         and N<sup>(C)</sup>).</i> This fingerprint
+        is a slight alteration of the fingerprint used in our previous publication.
+        Instead of combining two distances and two angles we now use one distance
+	and three angles. Analysis of the previous fingerprint revealed that the score
+	was slightly biased towards that the residue which angle was measured. By
+	using angles on both stem residues, the resulting score does not privilege
+	the fit of the candidate fragment to one stem residue over the other any
+	more.</p>
+	
+        <div align=center>
+        <img src="pictures/fingerprint.png" width="40%" height="40%" alt="logo">
+        </div>    
+       
+</p>   <h2>Score</h2>
+    The 500 best loop candidates are ranked according to a score that is composed
+    by a measure for the sequence identity  and the fit of the stem atoms of loop
+    and gap.<br>
+    <br>
+     <b>score = M - 0.1 * RMSD_stem<sup>2</sup></b><br>
+     <br>
+	
+	The sequence score M is calculated using an Environment-specific amino
+	acid substitution matrix for accessible residues. The stem score RMSD
+	is the root mean square deviation of the stem atoms C&alpha; and C of
+	the N-terminal stem residue and N and C&alpha; of the C-terminal stem residues.
+	
+	
         <h2>Fragment Search</h2>
-        <p>For the fragment search a stepwise approach is used
-        to minimize the search time as far as possible. In a
-        first step all fragments with the required sequence
-        length that fit to the stem atoms with an accuracy of
-        at least 0.75 &Aring; RMSD are selected. For the second step the 500 best
-        fitting candidates are chosen based on a quick estimation
-        of the steric fit of the fragment to the rest of the protein.
-        Subsequently, these 500 fragment are ranked by a score that
-        takes account of sequence similarity between the fragment and the
-        target sequence as well as the fit of the geometrical fingerprint
-        of the fragment to the stem atoms. To guarantee that the results describe
-        the most suitable conformational space, fragments with an
-        identical sequence or a backbone RMSD smaller than 1 &Aring; are removed
-        from the resulting list.</p>
+	
+        <p>To minimize the calculation time of the fragment search a stepwise
+	approach is used. In a first step, all fragments with the defined sequence
+	length and which stem atoms matching the stem atoms of the gap with  at least
+	0.75 &Aring RMSD are selected. In the second step, the 500 top candidates are chose
+	n based on a quick estimation of the steric fit of the fragment to the rest of
+	the protein e.g. excluding clashes. Subsequently, these 500 fragment are ranked
+	by a score calculated from thesequence similarity and matching of the geometrical
+	fingerprints of the (template) fragment and the target segment. To maximize the
+	conformational space, fragments with an identical sequence or a backbone RMSD
+	smaller than 1 &Aring are removed from the results list.</p>
         <div id="teaser" align="center">
         <img src="pictures/workflow.png" width="40%" height="40%" alt="logo" align=middle>
         </div>
